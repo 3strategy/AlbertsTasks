482cded012bb03ae46ad0a5923303e1a401d8d2e
al5493 <al5493@bs.amalnet.k12.il>
2024-10-25 17:46:05 +0300

    Ordered FBRef class

--- app/src/main/java/com/example/tasks/FBRef.java
+++ app/src/main/java/com/example/tasks/FBRef.java
@@ -6,18 +6,21 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class FBRef {
-    public static String uid;
+    public static FirebaseAuth refAuth=FirebaseAuth.getInstance();
+
     public static FirebaseDatabase FBDB = FirebaseDatabase.getInstance();
     public static DatabaseReference refUsers=FBDB.getReference("Users");
     public static DatabaseReference refTasks;
     public static DatabaseReference refDoneTasks;
     public static DatabaseReference refYears;
+
+    public static String uid;
+
     public static void getUser(FirebaseUser fbuser){
         uid = fbuser.getUid();
         refTasks=FBDB.getReference("Tasks").child(uid);
         refDoneTasks=FBDB.getReference("Done_Tasks").child(uid);
         refYears=FBDB.getReference("Years").child(uid);
     }
-    public static FirebaseAuth refAuth=FirebaseAuth.getInstance();
 
 }
